119c0bef60cfeef2ff255f8d02648586bbfa032c
Vaibhavkulshrestha12 <vaibhavkulshrestha55@gmail.com>
2024-06-03 00:44:32 +0530

    pop-up

--- main.js
+++ main.js
@@ -1,18 +1,40 @@
 const defaultFilters = [
-	"*://*.doubleclick.net/*",
-	"*://partner.googleadservices.com/*",
-	"*://*.googlesyndication.com/*",
-	"*://*.google-analytics.com/*",
-	"*://creative.ak.fbcdn.net/*",
-	"*://*.adbrite.com/*",
-	"*://*.exponential.com/*",
-	"*://*.quantserve.com/*",
-	"*://*.scorecardresearch.com/*",
-	"*://*.zedo.com/*",
-]
+    "*://*.doubleclick.net/*",
+    "*://partner.googleadservices.com/*",
+    "*://*.googlesyndication.com/*",
+    "*://*.google-analytics.com/*",
+    "*://creative.ak.fbcdn.net/*",
+    "*://*.adbrite.com/*",
+    "*://*.exponential.com/*",
+    "*://*.quantserve.com/*",
+    "*://*.scorecardresearch.com/*",
+    "*://*.zedo.com/*",
+];
+
+const popUpFilters = [
+    "*://*.hootsuite.com/*",
+    "*://*.optinmonster.com/*",
+    "*://*.wppopupmaker.com/*",
+];
 
 chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {return {cancel:true}},
-    { urls: defaultFilters},
+    function(details) {
+        return { cancel: true };
+    },
+    { urls: defaultFilters },
     ["blocking"]
-)
+);
+
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+        return { cancel: true };
+    },
+    { urls: popUpFilters },
+    ["blocking"]
+);
+
+chrome.windows.onCreated.addListener(function(window) {
+    if (window.type === 'popup') {
+        chrome.windows.remove(window.id);
+    }
+});
